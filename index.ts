41fd485c41a320ee611419333ecc4b828c3fe88a
Ronan McCarter <63772591+rpmccarter@users.noreply.github.com>
2024-06-04 12:52:43 -0700

    add marks

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose, { Schema, Types } from 'mongoose';
 
+// MARK: setup
 const mongoServer = await MongoMemoryServer.create();
 await mongoose.connect(mongoServer.getUri());
 mongoose.set({ debug: true });
 
+// MARK: create model
 type AddressInfo = {
   type: 'po-box';
   boxNumber: number;
@@ -36,6 +38,7 @@ UserSchema.path<Schema.Types.Subdocument>('address').discriminator(
 
 const User = mongoose.model<User>('User', UserSchema, 'users');
 
+// MARK: bug repro
 const user = await User.create({
   name: 'foo',
   address: {
@@ -52,5 +55,6 @@ await User.findByIdAndUpdate(user._id, { $set: { 'address.lastEmptied': 123 } })
 const updatedUser = await User.findById(user._id);
 console.log(updatedUser);
 
+// MARK: cleanup
 await mongoose.disconnect();
 await mongoServer.stop();
